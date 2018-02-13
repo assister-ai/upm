f4c807184c3e87406e6c4eaee84da951ceae83e6
farhoud <farhoud.m7@gmail.com>
2018-02-13 15:04:41 +0330

    - add package meta

---
+++ upm/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.0.1'
+
+__title__ = 'upm'
+__description__ = 'Universal Package Manager'
+__uri__ = 'https://github.com/assister-ai/upm'
+__doc__ = __description__ + ' <' + __uri__ + '>'
+
+__author__ = 'Keyvan Mir Mohammad Sadeghi, Mehdi Balouchi, Farhoud Mojahedzadeh'
+__email__ = 'keyvan.m.sadeghi@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2017 Assister AI'
+
+alive = "It's alive!"
+
+__all__ = [
+    'alive'
+]
+
