a8c0fffb2f615648dec33e117f837879dd5cc217
farhoud <farhoud.m7@gmail.com>
2018-03-07 12:45:52 +0330

    - add network operator class that handle networking - add NetworkConfig NetworkConfigs configs for keeping network config - facade NetworkConfig NetworkConfigs in ModuleNode - add NetworkStructure and NetworkStructures for managing networking compose tags - add routing for handling network in ModuleTree

--- upm/package/tree.py
+++ upm/package/tree.py
@@ -11,12 +11,15 @@ from common.const import MODULE_FOLDER
 from common.const import SPEC_FILE_NAME
 from package.specification import PackageSpecification
 
+from package.networking import NetworkOperatorBase
+
 log = logging.getLogger(__name__)
 
 
 class ModuleTree:
     def __init__(self, root):
         self.root = root
+        self.network_operator = NetworkOperatorBase()
 
     @classmethod
     def installer(cls, root_path):
@@ -33,13 +36,20 @@ class ModuleTree:
         return LevelOrderIter(self.root)
 
     def get_compose_dict(self):
+        network_structure = self.network_operator.config_network(LevelOrderIter(self.root))
+        log.debug("root network config")
+        log.debug(self.root.network_configs)
         services = {}
         for node in LevelOrderIter(self.root):
+            log.debug("dont fuck with me")
             services.update(node.get_compose_service())
-        return {'version': '3.3', 'services': services}
+        return {'version': '3.3', 'services': services, 'networks': network_structure.to_compose()}
 
     def ascii_art(self):
         print(RenderTree(self.root, style=AsciiStyle()))
+
+    def set_network_operator(self, network_operator):
+        self.network_operator = network_operator
 
 
 def modules_maker(root_path, parent=None):
@@ -68,9 +78,13 @@ class ModuleNode(NodeMixin):
         self.specification = PackageSpecification.from_yaml(os.path.join(specification_path, SPEC_FILE_NAME))
         self.abs_module_dir = os.path.join(self.abs_path, MODULE_FOLDER)
         self.name = self.specification.name
+        self.network_configs = NetworkConfigs()
 
     def install(self):
         ensure_makedir(self.abs_module_dir)
+
+    def add_network_config(self, network_name, name):
+        self.network_configs.add_alias(network_name, name)
 
     def fetch_dependencies(self):
         dependencies = self.specification.dependencies
@@ -98,8 +112,62 @@ class ModuleNode(NodeMixin):
         service_name = '_'.join(names)
         return service_name
 
+    def get_package_name(self):
+        return self.name
+
+    def get_name(self):
+        return self.name
+
     def get_compose_service(self):
-        return self.specification.to_compose_service(self.get_service_name())
+        service = self.specification.to_compose_service(self.get_service_name())
+        key = list(service.keys())[0]
+        value = list(service.values())[0]
+        value["networks"] = self.network_configs.to_compose_service_network()
+        return {key: value}
 
     def __repr__(self):
         return '{}'.format(self.name)
+
+
+class NetworkConfigs:
+    def __init__(self):
+        self.configs = {}
+
+    def add_network(self, network):
+        self.configs[network.name] = network
+
+    def add_alias(self, network_name, alias):
+        if network_name not in self.configs.keys():
+            self.add_network(NetworkConfig(network_name, [alias]))
+        else:
+            self.configs[network_name].add_alias(alias)
+
+    def to_compose_service_network(self):
+        result = {}
+        for config in self.configs.values():
+            log.debug(config)
+            log.debug(config.to_compose())
+            result.update(config.to_compose())
+        log.debug(result)
+        return result
+
+
+class NetworkConfig:
+    def __init__(self, name, aliases=None):
+        if aliases is None:
+            aliases = []
+        self.name = name
+        self.aliases = aliases
+
+    def add_alias(self, alias):
+        self.aliases.append(alias)
+
+    def get_aliases(self):
+        return self.aliases
+
+    def get_name(self):
+        return self.name
+
+    def to_compose(self):
+        log.debug(type(self.get_aliases()))
+        return {self.get_name(): {'aliases': self.get_aliases()}}
